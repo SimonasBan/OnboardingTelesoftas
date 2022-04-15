6115af243986aa7b6d501526ea6dcdadc2e2406b
SimonasBan <43903095+SimonasBan@users.noreply.github.com>
2022-04-15 09:39:33 +0300

    Move constants to appsetting.json

--- OnboardingWeatherAPI/OnboardingWeatherDOMAIN/Models/CityForecaster.cs
+++ OnboardingWeatherAPI/OnboardingWeatherDOMAIN/Models/CityForecaster.cs
@@ -7,6 +7,10 @@ namespace OnboardingWeatherDOMAIN.Models
     public class CityForecaster
     {
         //TODO: Think about how more cities or more forecasters can be added
+        /*Ideas:
+         * form,
+         * Add a file with cities
+         */
 
         //TODO: providerCityId
         public string AcceessItem { get; set; }
