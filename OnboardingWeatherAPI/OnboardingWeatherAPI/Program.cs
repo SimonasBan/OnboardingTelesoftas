8f9242989193a1635bee559659317965beefe86f
SimonasBan <43903095+SimonasBan@users.noreply.github.com>
2022-04-29 10:23:35 +0300

    Setup serilog

--- OnboardingWeatherAPI/OnboardingWeatherAPI/Program.cs
+++ OnboardingWeatherAPI/OnboardingWeatherAPI/Program.cs
@@ -9,37 +9,54 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration;
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-//Commented for identity because it declares it as well
-//----
-//// Add services to the container.
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+Log.Information("Starting up");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    ConfigurationManager configuration = builder.Configuration;
 
-//Swagger for jwt
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
+    //Commented for identity because it declares it as well
+    //----
+    //// Add services to the container.
+    //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    //    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+
+    //For serilog
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
+    //.WriteTo.Seq("http://localhost:5341"));
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    //Swagger for jwt
+    builder.Services.AddSwaggerGen(c =>
     {
-        Title = "JWTToken_Auth_API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "JWTToken_Auth_API",
+            Version = "v1"
+        });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement {
         {
             new OpenApiSecurityScheme {
                 Reference = new OpenApiReference {
@@ -50,82 +67,94 @@ builder.Services.AddSwaggerGen(c => {
             new string[] {}
         }
     });
-});
+    });
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//Identity
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-//                .AddEntityFrameworkStores<ApplicationDbContext>();
+    //Identity
+    //builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    //                .AddEntityFrameworkStores<ApplicationDbContext>();
 
-//---start---Identity V2
-// For Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+    //---start---Identity V2
+    // For Identity
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
-// Adding Authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-// Adding Jwt Bearer
-.AddJwtBearer(options =>
- {
-     options.SaveToken = true;
-     options.RequireHttpsMetadata = false;
-     options.TokenValidationParameters = new TokenValidationParameters()
+    // Adding Authentication
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    // Adding Jwt Bearer
+    .AddJwtBearer(options =>
      {
-         ValidateIssuer = true,
-         ValidateAudience = true,
-         ValidAudience = configuration["JWT:ValidAudience"],
-         ValidIssuer = configuration["JWT:ValidIssuer"],
-         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
-     };
- });
-//---end
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddHttpClient("OpenWeather", httpClient =>
-{
-    httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/");
-});
+         options.SaveToken = true;
+         options.RequireHttpsMetadata = false;
+         options.TokenValidationParameters = new TokenValidationParameters()
+         {
+             ValidateIssuer = true,
+             ValidateAudience = true,
+             ValidAudience = configuration["JWT:ValidAudience"],
+             ValidIssuer = configuration["JWT:ValidIssuer"],
+             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+         };
+     });
+    //---end
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    builder.Services.AddHttpClient("OpenWeather", httpClient =>
+    {
+        httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/");
+    });
 
 
-builder.Services.AddScoped<CityWeatherService>();
-builder.Services.AddScoped<OpenWeatherWeatherService>();
-builder.Services.AddScoped<CityService>();
-builder.Services.AddHostedService<ConsumeScopedServiceHostedService>();
-builder.Services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+    builder.Services.AddScoped<CityWeatherService>();
+    builder.Services.AddScoped<OpenWeatherWeatherService>();
+    builder.Services.AddScoped<CityService>();
+    builder.Services.AddHostedService<ConsumeScopedServiceHostedService>();
+    builder.Services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
 
-//FOR multiple dependency injection
-builder.Services.Scan(scan => scan
-              .FromAssemblyOf<IWeatherForecastService>()
-                .AddClasses(classes => classes.AssignableTo<IWeatherForecastService>())
-                    .AsImplementedInterfaces()
-                    .WithTransientLifetime()
-            );
+    //FOR multiple dependency injection
+    builder.Services.Scan(scan => scan
+                  .FromAssemblyOf<IWeatherForecastService>()
+                    .AddClasses(classes => classes.AssignableTo<IWeatherForecastService>())
+                        .AsImplementedInterfaces()
+                        .WithTransientLifetime()
+                );
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    //
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        //
+        app.UseDeveloperExceptionPage();
+        app.UseMigrationsEndPoint();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseSerilogRequestLogging();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
